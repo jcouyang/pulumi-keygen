6133053d00e05feee3e36f3fa6de9d0fe81a9e84
Jichao Ouyang <oyanglulu@gmail.com>
2026-04-17 16:19:47 +1000

    update nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/aaca8b07c7ba.tar.gz") {}}:
+{pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/1766437c5509.tar.gz") {}}:
 with builtins;
 
 let pulumi = pkgs.pulumi-bin.overrideDerivation (o: {
